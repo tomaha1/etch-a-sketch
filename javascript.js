73f420ea07136cbdc64f6bdec9aab98bb685cfeb
tomaha1 <tomaha1@gazeta.pl>
2023-01-31 20:03:11 +0100

    Add function to build grid of divs, update javascript.js and styles.css

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 let container = document.querySelector('.container');
+let max = 10;
 
-for (let i=1; i<=4; i++) {
-    let div = document.createElement('div');
-    div.classList.add('box');
-    container.appendChild(div);
+for (let i=1; i<=max; i++) {
+    let div1 = document.createElement('div');
+    div1.classList.add('box1');
+    container.appendChild(div1);
+        for (let i=1; i<=max; i++) {
+            let div2 = document.createElement('div');
+            div2.classList.add('box2');
+            div1.appendChild(div2);
+        }
 };
+
